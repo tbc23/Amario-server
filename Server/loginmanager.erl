a92b7ce2f27704f0561423f7e2d8c3f71f0b13cf
Gilberto Cunha <gilbertocunha@pop-os.localdomain>
2021-05-13 23:06:52 +0100

    descentralize loginmanager from server

--- Server/loginmanager.erl
+++ Server/loginmanager.erl
@@ -1,12 +1,57 @@
 -module(loginmanager).
--export([start/0, stop/1, create_account/2, close_account/2, login/2, logout/2, online/1, loop/1, leaderboard/1]).
+-export([start/1, stop/1, parse_requests/1]).
 
-start () -> register(?MODULE, spawn(fun() -> loop(dict:new()) end)).
+start (Port) -> spawn(fun() -> lobby(Port) end).
+stop (Server) -> Server ! stop.
+
+lobby (Port) ->
+	{ok, LSock} = gen_tcp:listen(Port, [binary, {packet, line}, {active, true}]),
+	spawn(fun() -> startlm() end),
+	spawn(fun() -> acceptor(LSock) end),
+	receive stop -> ok end.
+
+acceptor (LSock) ->
+	{ok, Sock} = gen_tcp:accept(LSock),
+	spawn(fun() -> acceptor(LSock) end),	
+	io:format("User entered lobby~n", []),
+	parse_requests (Sock).
+
+parse_requests (Sock) ->
+	gen_tcp:send(Sock, list_to_binary("Waiting for request:\n")),
+	receive {tcp, _, BinMsg} ->
+		Msg = string:replace(binary_to_list(BinMsg), "\n", ""),
+		[Req | Args] = string:split(string:replace(Msg, ":", ""), " ", all),
+		case Req of 
+			"login" -> 
+				login(Sock, Args),
+				parse_requests (Sock);
+			"logout" -> 
+				logout(Sock, Args),
+				parse_requests (Sock);
+			"create_account" -> 
+				create_account(Sock, Args),
+				parse_requests (Sock);
+			"close_account" -> 
+				close_account(Sock, Args),
+				parse_requests (Sock);
+			"online" -> 
+				online(Sock),
+				parse_requests (Sock);
+			"leaderboard" -> 
+				leaderboard (Sock),
+				parse_requests (Sock);
+			"close" -> gen_tcp:close(Sock);
+			_ -> 
+				gen_tcp:send(Sock, list_to_binary("Invalid request: " ++ Req ++ "\n")),
+				parse_requests (Sock)
+		end
+	end.
+
+startlm () -> register(?MODULE, spawn(fun() -> loop(dict:new()) end)).
 
 server_call (Sock, Request) ->
 	?MODULE ! {Request, self()},
 	receive {Res, ?MODULE} -> 
-		io:format("Response: ~p~n", [Res]),
 		case is_atom(Res) of 
 			true -> R = atom_to_list(Res);
 			false -> R = Res
@@ -16,17 +61,14 @@ server_call (Sock, Request) ->
 
 create_account (Sock, Args) -> 
 	[User, Pass] = Args,
-	io:format("Req: Create Account | User: ~p | Pass: ~p~n", [User, Pass]),
 	server_call (Sock, {create_account, User, Pass}).
 
 close_account (Sock, Args) -> 
 	[User, Pass] = Args,
-	io:format("Req: Close Account | User: ~p | Pass: ~p~n", [User, Pass]),
 	server_call (Sock, {close_account, User, Pass}).
 
 login (Sock, Args) -> 
 	[User, Pass] = Args,
-	io:format("Req: Login | User: ~p | Pass: ~p~n", [User, Pass]),
 	server_call (Sock, {login, User, Pass}).
 
 logout (Sock, Args) -> 
@@ -36,7 +78,6 @@ logout (Sock, Args) ->
 
 leaderboard (Sock) -> server_call (Sock, leaderboard).
 online (Sock) -> server_call (Sock, online).
-stop (Sock) -> server_call (Sock, stop).
 
 loop (Users) ->
 	receive 
